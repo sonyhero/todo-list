f2fc02851d34c4288b8738bf0acd3e0f7027690c
sonyhero <27anton06@gmail.com>
2023-04-05 14:27:36 +0300

    lesson06-add-CRUD-operations(functions)-props-to-Todolist-component

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,67 +1,55 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import React from 'react';
 import {FilterValuesType, TaskType} from './App'
 import {useAutoAnimate} from '@formkit/auto-animate/react';
 import s from './Todolist.module.css'
+import {AddItemForm} from './components/AddItemForm';
 
 type PropsType = {
     title: string
     todoListId: string
     tasks: TaskType[]
-    removeTask: (taskId: string, todoListId: string) => void
-    changeFilter: (value: FilterValuesType, todoListId: string) => void
-    addTask: (newTitle: string, todoListId: string) => void
-    changeIsDone: (taskId: string, newIsDone: boolean, todoListId: string) => void
     filter: FilterValuesType
+
+    addTask: (newTitle: string, todoListId: string) => void
+    removeTask: (taskId: string, todoListId: string) => void
+    changeTaskTitle: (taskId: string, newTitle: string, todoListId: string) => void
+    changeTaskStatus: (taskId: string, newIsDone: boolean, todoListId: string) => void
+
+    addTodoList: (newTitle: string) => void
+    changeTodoListTitle: (newTitle: string, todoListId: string) => void
     removeTodoList: (todoListId: string) => void
+    changeTodoListFilter: (value: FilterValuesType, todoListId: string) => void
+
 }
 
-export function Todolist(props: PropsType) {
-
-    const [newTitle, setNewTitle] = useState('')
-    const [error, setError] = useState<string | null>(null)
+export const Todolist: React.FC<PropsType> = (props) => {
 
     const [listRef] = useAutoAnimate<HTMLUListElement>()
 
-    const addTaskHandler = () => {
-        if (newTitle.trim() !== '') {
-            props.addTask(newTitle.trim(), props.todoListId)
-            setNewTitle('')
-        } else {
-            setError('Ошибка')
-        }
-    }
-    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-        setError(null)
-        if (event.key === 'Enter') addTaskHandler()
-    }
+    const removeTaskHandler = (taskId: string) => props.removeTask(taskId, props.todoListId)
 
-    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        setNewTitle(event.currentTarget.value)
-    }
 
-    const removeTaskHandler = (taskId: string) => {
-        props.removeTask(taskId, props.todoListId)
-    }
+    const changeFilter = (filerValue: FilterValuesType) => props.changeTodoListFilter(filerValue, props.todoListId)
 
-    const changeFilter = (filerValue: FilterValuesType) => {
-        props.changeFilter(filerValue, props.todoListId)
-    }
 
-    const onChangeIsDoneHandler = (taskId: string, e: boolean) => {
-        props.changeIsDone(taskId, e, props.todoListId)
-    }
+    const onChangeIsDoneHandler = (taskId: string, e: boolean) => props.changeTaskStatus(taskId, e, props.todoListId)
+
+    // const addTask = (newTitle: string, todoListId: string) => {
+    //     props.addTask(newTitle, todoListId)
+    // }
 
     return <div>
         <h3>{props.title}</h3>
-        <div>
-            <input value={newTitle}
-                   onChange={onChangeHandler}
-                   onKeyDown={onKeyDownHandler}
-                   className={error ? s.error : ''}
-            />
-            <button onClick={addTaskHandler}>+</button>
-            {error && <div className={s.errorMessage}>{error}</div>}
-        </div>
+        <AddItemForm addItem={props.addTask} todoListId={props.todoListId}/>
+        {/*<div>*/}
+        {/*    <input value={newTitle}*/}
+        {/*           onChange={onChangeHandler}*/}
+        {/*           onKeyDown={onKeyDownHandler}*/}
+        {/*           className={error ? s.error : ''}*/}
+        {/*    />*/}
+        {/*    <button onClick={addTaskHandler}>+</button>*/}
+        {/*    {error && <div className={s.errorMessage}>{error}</div>}*/}
+        {/*</div>*/}
 
         <ul ref={listRef}>
             {
@@ -79,9 +67,15 @@ export function Todolist(props: PropsType) {
             }
         </ul>
         <div>
-            <button className={props.filter=== 'all' ? s.activeFilter : ''} name={'All'} onClick={() => changeFilter('all')}>All</button>
-             <button className={props.filter=== 'active' ? s.activeFilter : ''} name={'Active'} onClick={() => changeFilter('active')}>Active</button>
-             <button className={props.filter=== 'completed' ? s.activeFilter : ''} name={'Completed'} onClick={() => changeFilter('completed')}>Completed</button>
+            <button className={props.filter === 'all' ? s.activeFilter : ''} name={'All'}
+                    onClick={() => changeFilter('all')}>All
+            </button>
+            <button className={props.filter === 'active' ? s.activeFilter : ''} name={'Active'}
+                    onClick={() => changeFilter('active')}>Active
+            </button>
+            <button className={props.filter === 'completed' ? s.activeFilter : ''} name={'Completed'}
+                    onClick={() => changeFilter('completed')}>Completed
+            </button>
         </div>
     </div>
 }
