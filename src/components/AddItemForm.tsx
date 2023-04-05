c191e0cc9a879223ce4d1a3d15aecf1846f38765
sonyhero <27anton06@gmail.com>
2023-04-05 14:28:05 +0300

    lesson06-add-AddItemForm-component

---
+++ src/components/AddItemForm.tsx
@@ -0,0 +1,40 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import s from '../Todolist.module.css';
+
+type AddItemFormPropsType = {
+    addItem: (newTitle: string, todoListId: string) => void
+    todoListId: string
+}
+export const AddItemForm: React.FC<AddItemFormPropsType> = (props) => {
+
+    const [newTitle, setNewTitle] = useState('')
+    const [error, setError] = useState<string | null>(null)
+
+    const onKeyDownHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+        setError(null)
+        if (event.key === 'Enter') addTaskHandler()
+    }
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTitle(event.currentTarget.value)
+    }
+    const addTaskHandler = () => {
+        if (newTitle.trim() !== '') {
+            props.addItem(newTitle.trim(), props.todoListId)
+            setNewTitle('')
+        } else {
+            setError('Ошибка')
+        }
+    }
+    return (
+        <div>
+            <input value={newTitle}
+                   onChange={onChangeHandler}
+                   onKeyDown={onKeyDownHandler}
+                   className={error ? s.error : ''}
+            />
+            <button onClick={addTaskHandler}>+</button>
+            {error && <div className={s.errorMessage}>{error}</div>}
+        </div>
+    )
+}
