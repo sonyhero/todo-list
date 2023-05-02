f35937126217491bd6a92414527b605d928d31cf
sonyhero <27anton06@gmail.com>
2023-05-02 08:56:22 +0300

    add-useAppSelector-useDispatch

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,4 +1,5 @@
-import {TypedUseSelectorHook, useSelector} from 'react-redux';
+import {TypedUseSelectorHook, useDispatch, useSelector} from 'react-redux';
 import {RootStateType} from '../redux-store/store';
 
+// export const useAppDispatch = () => useDispatch<AppDispatch>() // для санок
 export const useAppSelector: TypedUseSelectorHook<RootStateType> = useSelector
--- src/redux-store/store.ts
+++ src/redux-store/store.ts
@@ -1,6 +1,8 @@
-import {combineReducers, legacy_createStore as createStore} from 'redux';
+import {AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from 'redux';
 import {TodoListsReducer} from '../reducers/todoListsReducer';
 import {TasksReducer} from '../reducers/tasksReducer';
+import thunk, {ThunkAction, ThunkDispatch} from 'redux-thunk';
+
 
 
 export const rootReducer = combineReducers({
@@ -8,6 +10,15 @@ export const rootReducer = combineReducers({
     tasks: TasksReducer
 })
 
-export type RootStateType = ReturnType<typeof rootReducer>
+// export type RootReducerType = ReturnType<typeof rootReducer>
+// export type AppDispatch = ThunkDispatch<RootStateType, unknown, AnyAction> // для санок
+// export type AppThunk<ReturnType = void> = ThunkAction<
+// ReturnType,
+//     RootStateType,
+//     unknown,
+//     AnyAction
+// >
+export type RootStateType = ReturnType<typeof store.getState>
 
+// export const store = createStore(rootReducer, applyMiddleware(thunk)) // для санок
 export const store = createStore(rootReducer)
