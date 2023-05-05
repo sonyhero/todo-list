8ade0338925192cb85fe8ae340ee7532076ba5ed
sonyhero <27anton06@gmail.com>
2023-05-05 14:27:34 +0300

    App-refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 import './App.css';
 import {Todolist} from './Todolist';
 import {AddItemForm} from './components/AddItemForm/AddItemForm';
-import {
-    AddTaskAC,
-    ChangeTaskStatusAC,
-    ChangeTaskTitleAC,
-    RemoveTaskAC,
-} from './reducers/tasksReducer';
-import {
-    AddTodoListAC,
-    ChangeTodoListFilterAC,
-    ChangeTodoListTitleAC,
-    RemoveTodoListAC,
-} from './reducers/todoListsReducer';
+import {AddTodoListAC} from './reducers/todoListsReducer';
 import {useAutoAnimate} from '@formkit/auto-animate/react';
 import {useAppSelector} from './hooks/hooks';
 import {useDispatch} from 'react-redux';
@@ -33,58 +22,23 @@ export type TasksStateType = {
     [key: string]: TaskType[]
 }
 
-function App() {
+export const App = () => {
 
     const [todoListsRef] = useAutoAnimate<HTMLDivElement>()
 
     const todoLists = useAppSelector(state => state.todoLists)
     const dispatch = useDispatch()
-
-    // CRUD operations for Tasks
-    const addTask = (newTitle: string, todoListId: string) => {
-        dispatch(AddTaskAC(newTitle, todoListId))
-    }
-    const removeTask = (taskId: string, todoListId: string) => {
-        dispatch(RemoveTaskAC(taskId, todoListId))
-    }
-    const changeTaskTitle = (taskId: string, newTitle: string, todoListId: string) => {
-
-        dispatch(ChangeTaskTitleAC(taskId, newTitle, todoListId))
-    }
-    const changeTaskStatus = (taskId: string, newIsDone: boolean, todoListId: string) => {
-        dispatch(ChangeTaskStatusAC(taskId, newIsDone, todoListId))
-    }
-    // CRUD operations for TodoLists
     const addTodoList = (newTitle: string) => {
         dispatch(AddTodoListAC(newTitle))
     }
-    const removeTodoList = (todoListId: string) => {
-        dispatch(RemoveTodoListAC(todoListId))
-    }
-    const changeTodoListTitle = (newTitle: string, todoListId: string) => {
-        dispatch(ChangeTodoListTitleAC(newTitle, todoListId))
-    }
-    const changeTodoListFilter = (filerValue: FilterValuesType, todoListId: string) => {
-        dispatch(ChangeTodoListFilterAC(filerValue, todoListId))
-    }
 
     const todoListsComponents = todoLists.map(tl => {
-
             return (
                 <Todolist
                     key={tl.id}
                     todoListId={tl.id}
                     title={tl.title}
                     filter={tl.filter}
-
-                    addTask={addTask}
-                    removeTask={removeTask}
-                    changeTaskTitle={changeTaskTitle}
-                    changeTaskStatus={changeTaskStatus}
-
-                    removeTodoList={removeTodoList}
-                    changeTodoListTitle={changeTodoListTitle}
-                    changeTodoListFilter={changeTodoListFilter}
                 />
             )
         }
@@ -97,8 +51,5 @@ function App() {
                 {todoListsComponents}
             </div>
         </div>
-
-    );
+    )
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import {store} from './redux-store/store';
@@ -13,7 +13,6 @@ root.render(
     <Provider store={store}>
         <App />
     </Provider>
-
 );
 
 // If you want to start measuring performance in your app, pass a function
