83f780cf208643dd1d52268564f36e0acfe3b3b9
sonyhero <27anton06@gmail.com>
2023-05-05 14:27:55 +0300

    Todolist-refactoring

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -6,28 +6,42 @@ import {AddItemForm} from './components/AddItemForm/AddItemForm';
 import {Button} from './components/Button/Button';
 import {EditableSpan} from './components/EditableSpan';
 import {useAppSelector} from './hooks/hooks';
+import {useDispatch} from 'react-redux';
+import {
+    ChangeTodoListFilterAC,
+    ChangeTodoListTitleAC,
+    RemoveTodoListAC
+} from './reducers/todoListsReducer';
+import {AddTaskAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveTaskAC} from './reducers/tasksReducer';
 
 type TodolistPropsType = {
     title: string
     todoListId: string
-    // tasks: TaskType[]
     filter: FilterValuesType
-
-    addTask: (newTitle: string, todoListId: string) => void
-    removeTask: (taskId: string, todoListId: string) => void
-    changeTaskTitle: (taskId: string, newTitle: string, todoListId: string) => void
-    changeTaskStatus: (taskId: string, newIsDone: boolean, todoListId: string) => void
-
-    changeTodoListTitle: (newTitle: string, todoListId: string) => void
-    removeTodoList: (todoListId: string) => void
-    changeTodoListFilter: (value: FilterValuesType, todoListId: string) => void
 }
 
 export const Todolist: React.FC<TodolistPropsType> = (props) => {
 
-    const tasks = useAppSelector(state => state.tasks)
+    const {title, todoListId, filter} = props
+
     const [listRef] = useAutoAnimate<HTMLUListElement>()
 
+    const tasks = useAppSelector(state => state.tasks[todoListId])
+    const dispatch = useDispatch()
+
+    // CRUD operations for TodoLists
+    const removeTodoList = () => {
+        dispatch(RemoveTodoListAC(todoListId))
+    }
+    const changeTodoListTitle = (newTitle: string) => {
+        dispatch(ChangeTodoListTitleAC(newTitle, todoListId))
+    }
+
+    const setChangeFilterAll = () => dispatch(ChangeTodoListFilterAC(todoListId, 'all'))
+    const setChangeFilterActive = () => dispatch(ChangeTodoListFilterAC(todoListId, 'active'))
+    const setChangeFilterCompleted = () => dispatch(ChangeTodoListFilterAC(todoListId, 'completed'))
+
+    // CRUD operations for Tasks
     const tasksForTodolist = (taskList: TaskType[], filterValue: FilterValuesType) => {
         return (filterValue === 'active')
             ? taskList.filter(t => !t.isDone)
@@ -35,46 +49,37 @@ export const Todolist: React.FC<TodolistPropsType> = (props) => {
                 ? taskList.filter(t => t.isDone)
                 : taskList
     }
-    const tasksForRender = tasksForTodolist(tasks[props.todoListId], props.filter)
-
-    const removeTaskHandler = (taskId: string) => props.removeTask(taskId, props.todoListId)
-
-    const setChangeFilterAll = () => props.changeTodoListFilter('all', props.todoListId)
-    const setChangeFilterActive = () => props.changeTodoListFilter('active', props.todoListId)
-    const setChangeFilterCompleted = () => props.changeTodoListFilter('completed', props.todoListId)
-
-    const onChangeIsDoneHandler = (taskId: string, e: boolean) => props.changeTaskStatus(taskId, e, props.todoListId)
+    const tasksForRender = tasksForTodolist(tasks, filter)
 
     const addTask = (newTitle: string) => {
-        props.addTask(newTitle, props.todoListId)
+        dispatch(AddTaskAC(todoListId, newTitle))
+    }
+    const removeTaskHandler = (taskId: string) => {
+        dispatch(RemoveTaskAC(todoListId, taskId))
+    }
+    const onChangeTaskStatusHandler = (taskId: string, e: boolean) => {
+        dispatch(ChangeTaskStatusAC(todoListId, taskId, e,))
     }
 
-    const removeTodoList = () => {
-      props.removeTodoList(props.todoListId)
-    }
-    const changeTodoListTitle = (newTitle: string) => {
-        props.changeTodoListTitle(newTitle, props.todoListId)
+    const changeTaskTitle = (taskId: string, newTitle: string) => {
+        dispatch(ChangeTaskTitleAC(todoListId, taskId, newTitle))
     }
 
-    return <div  className={s.todoWrapper}>
+    return <div className={s.todoWrapper}>
         <h3>
-            <EditableSpan title={props.title} onChange={changeTodoListTitle}/>
-            <Button name={'x'} callback={removeTodoList}  xType={'red'} className={false}/>
+            <EditableSpan title={title} onChange={changeTodoListTitle}/>
+            <Button name={'x'} callback={removeTodoList} xType={'red'} className={false}/>
         </h3>
         <AddItemForm addItem={addTask}/>
         <ul className={s.tasks} ref={listRef}>
             {
                 tasksForRender.map(t => {
-                    const changeTaskTitle = (newTitle: string) => {
-                        props.changeTaskTitle(t.id, newTitle, props.todoListId)
-                    }
-
                     return (
                         <li className={t.isDone ? s.isDone : ''} key={t.id}>
                             <input type="checkbox"
-                                   onChange={(e) => onChangeIsDoneHandler(t.id, e.currentTarget.checked)}
+                                   onChange={(e) => onChangeTaskStatusHandler(t.id, e.currentTarget.checked)}
                                    checked={t.isDone}/>
-                            <EditableSpan onChange={changeTaskTitle} title={t.title}/>
+                            <EditableSpan onChange={(newTitle) => changeTaskTitle(t.id, newTitle)} title={t.title}/>
                             <Button name={'x'}
                                     callback={() => removeTaskHandler(t.id)}
                                     xType={'red'}
@@ -84,9 +89,9 @@ export const Todolist: React.FC<TodolistPropsType> = (props) => {
             }
         </ul>
         <div>
-            <Button className={props.filter === 'all'} name={'All'} callback={setChangeFilterAll}/>
-            <Button className={props.filter === 'active'} name={'Active'} callback={setChangeFilterActive}/>
-            <Button className={props.filter === 'completed'} name={'Completed'} callback={setChangeFilterCompleted}/>
+            <Button className={filter === 'all'} name={'All'} callback={setChangeFilterAll}/>
+            <Button className={filter === 'active'} name={'Active'} callback={setChangeFilterActive}/>
+            <Button className={filter === 'completed'} name={'Completed'} callback={setChangeFilterCompleted}/>
         </div>
     </div>
 }
