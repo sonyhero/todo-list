506f0bf1c7d4bc05b2bee9531c95021d33980f00
sonyhero <27anton06@gmail.com>
2023-05-25 13:33:02 +0300

    add-axios-and-api

---
+++ src/api/api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+    headers: {
+        'API-KEY': '275a40de-010f-4090-ae39-5e228c881a39'
+    }
+})
