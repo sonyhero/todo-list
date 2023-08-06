652a19e11492aeaba90d979d1762c41c2849e8b4
sonyhero <27anton06@gmail.com>
2023-08-06 12:01:29 +0300

    refactoring: error-utils

---
+++ src/utils/handle-server-app-error.ts
@@ -0,0 +1,12 @@
+import { ResponseType } from '../api/api'
+import { Dispatch } from 'redux'
+import {setAppError, setAppStatus} from '../app/app-reducer';
+
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
+  if (data.messages.length) {
+    dispatch(setAppError({ error: data.messages[0] }))
+  } else {
+    dispatch(setAppError({ error: 'Some error occurred' }))
+  }
+  dispatch(setAppStatus({ status: 'failed' }))
+}
---
+++ src/utils/handle-server-network-error.ts
@@ -0,0 +1,14 @@
+import { Dispatch } from 'redux'
+import axios, { AxiosError } from 'axios'
+import {setAppError, setAppStatus} from '../app/app-reducer';
+
+export const handleServerNetworkError = (e: unknown, dispatch: Dispatch) => {
+  const err = e as Error | AxiosError<{ error: string }>
+  if (axios.isAxiosError(err)) {
+    const error = err.message ? err.message : 'Some error occurred'
+    dispatch(setAppError({ error }))
+  } else {
+    dispatch(setAppError({ error: `Native error ${err.message}` }))
+  }
+  dispatch(setAppStatus({ status: 'failed' }))
+}
