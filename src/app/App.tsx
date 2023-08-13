7d61246e99170ffad5f81b183b21759dbbdc83d6
sonyhero <27anton06@gmail.com>
2023-08-13 17:47:49 +0300

    feat: add routing component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,10 @@
 import { useEffect } from 'react'
-import './App.css'
 import { useActions, useAppSelector } from '../common/hooks'
 import { selectIsInitialized } from './app-selectors'
-import { Error404, LinearProgress, Toast } from '../common/components'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { TodolistsList } from '../features/todolists-list/TodolistsList'
-import { Login } from '../features/auth/login'
+import { LinearProgress, Toast } from '../common/components'
 import { authThunks } from '../features/auth/auth-reducer'
 import 'react-toastify/dist/ReactToastify.css'
-
-export type FilterValuesType = 'all' | 'active' | 'completed'
+import { Routing } from './routing'
 
 export const App = () => {
   const isInitialized = useAppSelector(selectIsInitialized)
@@ -22,19 +17,7 @@ export const App = () => {
   return (
     <>
       <Toast />
-      {!isInitialized ? (
-        <LinearProgress />
-      ) : (
-        <div className={'App'}>
-          <Routes>
-            <Route path={'/'} element={<TodolistsList />} />
-            <Route path={'/login'} element={<Login />} />
-            <Route path={'/404'} element={<Error404 />} />
-            <Route path={'*'} element={<Navigate to={'/404'} />} />
-          </Routes>
-        </div>
-      )
-      }
+      {!isInitialized ? <LinearProgress /> : <Routing />}
     </>
   )
 }
---
+++ src/app/routing.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { TodolistsList } from '../features/todolists-list/TodolistsList'
+import { Login } from '../features/auth/login'
+import { Error404 } from '../common/components'
+
+export const Routing = () => {
+  return <div className={'App'}>
+    <Routes>
+      <Route path={'/'} element={<TodolistsList />} />
+      <Route path={'/login'} element={<Login />} />
+      <Route path={'/404'} element={<Error404 />} />
+      <Route path={'*'} element={<Navigate to={'/404'} />} />
+    </Routes>
+  </div>
+}
