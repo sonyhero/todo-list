7f9b1559a223a8c37cd22b39f9f6f8ac661c3f63
sonyhero <27anton06@gmail.com>
2023-08-19 12:52:13 +0300

    fix: Filter-button-block

--- src/features/todolists-list/Todolist/Filtered-buttun-block/Filter-button-block.tsx
+++ src/features/todolists-list/Todolist/Filtered-buttun-block/Filter-button-block.tsx
@@ -2,7 +2,7 @@ import { FC, memo } from 'react'
 import { useActions } from '@/common/hooks'
 import s from './Filter-button-block.module.scss'
 import { FilterValuesType, todolistActions } from '@/features/todolists-list/todoListsReducer'
-import { Button } from '@/components'
+import { Button } from '@/components/ui/button'
 
 type Props = {
   todolistId: string
@@ -22,8 +22,14 @@ export const FilterButtonBlock: FC<Props> = memo(({ filter, todolistId }) => {
     { name: 'Completed', filterValue: 'completed' },
   ]
 
+  // const mappedButton = data.map(({ name, filterValue }) => (
+  //   <Button key={name} name={name} className={filter === filterValue} callback={changeFilter(filterValue)} />
+  // ))
+
   const mappedButton = data.map(({ name, filterValue }) => (
-    <Button key={name} name={name} className={filter === filterValue} callback={changeFilter(filterValue)} />
+    <Button key={name} onClick={changeFilter(filterValue)} variant={filter === filterValue ? 'tertiary' : 'primary'}>
+      {name}
+    </Button>
   ))
 
   return <div className={s.btwWrap}>{mappedButton}</div>
