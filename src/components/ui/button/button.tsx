76282aee2a4e37d1db2501cbc56daa3287ef39de
sonyhero <27anton06@gmail.com>
2023-08-19 16:40:18 +0300

    feat: button

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -4,17 +4,15 @@ import s from './button.module.scss'
 
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
-  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'icon' | 'link'
   fullWidth?: boolean
   className?: string
 } & ComponentPropsWithoutRef<T>
 
 export const Button = <T extends ElementType = 'button'>(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>,
 ) => {
   const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
 
-  return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
-  )
+  return <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
 }
