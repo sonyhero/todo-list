bb1caceae0450cb2894aa8731e6f764dcad09593
sonyhero <27anton06@gmail.com>
2023-08-20 20:25:07 +0300

    feat: common api

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -5,5 +5,6 @@ export const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.1/',
   headers: {
     'API-KEY': '275a40de-010f-4090-ae39-5e228c881a39',
+    withCredentials: true,
   },
 })
