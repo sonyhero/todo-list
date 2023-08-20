41425f00dfbb8a4052c841fe328215a79b64d783
sonyhero <27anton06@gmail.com>
2023-08-20 20:13:49 +0300

    feat: add pages gh

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app/App'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/index.scss'
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
@@ -11,8 +11,8 @@ import '@fontsource/roboto/700.css'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 )
